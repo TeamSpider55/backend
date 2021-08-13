ce617910ced9664d3578085ff8d5e072eea2f885
Mei-Hung Lu <meihung.lu@student.unimelb.edu.au>
2021-08-13 18:14:28 +1000

    added some scripting for schema

---
+++ models/client.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const clientSchema = new mongoose.Schema({
+  userId: { type: String, unique: true }, //Foreign key only unique within the user(?)
+  nickName: { type: String },
+  familyName: { type: String, required: true },
+  middleName: { type: String },
+  givenName: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  address: { type: String },
+  description: { type: String, required: true },
+  tags: [{ type: String }],
+});
+
+const Client = mongoose.model("Client", clientSchema);
+module.exports = Client;
---
+++ models/tags.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const tagSchema = new mongoose.schema({
+  tagId: { type: String, required: true },
+  description: { type: String, required: true },
+  color: { type: String, required: true },
+  priority: { type: Number, require: true, min: 1, max: 5 },
+});
+
+const Tag = mongoose.model("Tag", tagSchema);
+module.exports = Tag;
