cba7a3011c861a1636c4f3e83972bfc96fd419ee
June-Tr <june-tr@outlook.com>
2021-08-23 05:45:38 +1000

    Update schedule controller
    this is only control the document page that record user and date
    ->basic  functionality: add, remove, simple search

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,37 +1,183 @@
+const { model } = require("mongoose");
 const Schedule = require("../models/schedules");
-require("../models/index");
-// validate method
-const setCategory = (cur, next) => {
-    if((next == "free" || cur != "allocated"))
-        throw new Error("Only able to request to free an allocated event!!!");
-    return next;
-}
+const Util = require("../util");
 
-// retrieve the scheduled date 
-// null value indicate there are no event on that day
-const retrieveSchedule = async (date) => {
-    try{
-        const schedule = await Schedule.findOne({date: date});
-        return schedule;
-    } catch(err){
-        return null;
+/* Controll I/O from schedule collection ****************************************************************************************************
+ */
+
+let scheduleController = {
+    
+    /* Helper function. ******************************************************************************************************************
+     */
+
+    /* Comparator of the schedule
+     * -> this can be use to re organise the retrieve schedule to do calender
+     * @param {Schedule} contain date
+     * @para, {Schedule} contain date
+     * @return {int} 1, if a > b ; -1, if a < b; 0 if a == b
+     */
+    compare : (scheduleA, scheduleB) => {
+        if(scheduleA.date < scheduleB.date)
+            return -1;
+        if(scheduleA.date > scheduleB.date)
+            return 1;
+        return 0;
+    },
+
+    /* Add schedule of the user to the data base (schedule collection)
+     * @param {Number} time in millis second that is between 0:0:0 -> 23:59:59.999 of the scheduled day
+     * @param {String} the user id where this schedule belong to
+     * @return {Schedule} the instance scheduleSchema that is stored in data base of null;
+     */
+    addSchedule : async (unixTime, concreteUser) => {
+        try{
+            return await Schedule.create({
+                date: Util.extractUnixOfYYYY_MM_DD(unixTime),
+                user: concreteUser,
+                events: []
+            });
+        }catch (err){
+            // notify that the new schedule is existed in the db. aka: error code E11000
+            if(err.message.includes("E11000 ")){
+                console.log("Duplicate document: Scheduled day {" + new Date(unixTime) + "}, for id: '"
+                 + concreteUser + "' is existed in Collection{Schedules}~!!!");
+            }else{
+                console.log(err);
+            }
+        } return null;
+    },
+
+
+    /* Retrieve the schedules document using date and user
+     * @param {Number} time in millis second that is between 0:0:0 -> 23:59:59.999 of the scheduled day
+     * @param {String} the user id where this schedule belong to
+     * @return {Schedule} that exist in collection or null
+     */
+    retrieveSchedule: async (unixTime, concreteUser) => {
+        try{
+            let schedule =  await Schedule.findOne({
+                date: Util.extractUnixOfYYYY_MM_DD(unixTime),
+                user: concreteUser
+            });
+
+            // log case targetted date is not in database
+            if(schedule == null)
+                console.log("Missing document: scheduled day {" + new Date(unixTime) + "}, for id: '" 
+                + concreteUser + "' is missing from Collection{Schedules}~!!!");
+            return schedule;
+            }catch (err){
+            console.log(err);
+        }return null;
+    },
+
+    /* Retrieve all the schedule belong to a user
+     * @param {String} the id of the user
+     * @return {Schedule[]} contain all the schedule day of the user
+     */
+    retrieveAllSchedule: async (concreteUser) => {
+        try{
+            let schedule =  await Schedule.find({
+                user: concreteUser
+            });
+
+            // log case targetted date is not in database
+            if(schedule == null)
+                console.log("Missing document: scheduled day {" + new Date(unixTime) + "}, for id: '" + concreteUser 
+                + "' is missing from Collection{Schedules}~!!!");
+            return schedule;
+        }catch (err){
+            console.log(err);
+        }return null;
+    },
+
+    /* Retrieve all the schedules that is between a indicated period of a user
+     * @param {String} the id of the user
+     * @param {Int} the unix time of the starting period
+     * @param {Int} the unix time of the ending period
+     * @return {schedule[]} 
+     */
+    retrieveAllSchedule: async(concreteUser, start, end) => {
+        try{
+            let schedules = await Schedule.find({
+                $and: [
+                    {"date": {$gte: Util.extractUnixOfYYYY_MM_DD(start)}},
+                    {"date": {$lte: Util.extractUnixOfYYYY_MM_DD(end)}}
+                ],
+                "user": concreteUser
+            });
+
+            if(schedules == null || schedules.length == 0){
+                console.log("No document: scheduled day that between:{" 
+                + new Date(Util.extractUnixOfYYYY_MM_DD(start)) + "} -> {" + 
+                new Date(Util.extractUnixOfYYYY_MM_DD(end)) + "}, for id: '" + concreteUser + "' contain no document~!!!");
+            }
+            return schedules;
+        }catch (err){
+            console.log(err);
+        }
+    },
+
+    /* Remove a schedule from the collection using time+ user
+     * @param {Int} the unix time of the schedule
+     * @param {String} the id of the user 
+     * @return {bool} true if the delete is successful
+     */
+    removeSchedule: async (unixTime, concreteUser) => {
+        
+        let report = await Schedule.deleteOne({
+            date: Util.extractUnixOfYYYY_MM_DD(unixTime),
+            user: concreteUser
+        });
+
+        if(report.n == 1)
+            return true;
+
+        if(report.ok != 1)
+            console.log("Error: occur when delete schedule at {" + new Date(Util.extractUnixOfYYYY_MM_DD(unixTime))) + 
+                    ", of id:" + concreteUser + "!!!!";
+
+        return false;
+    },
+
+    /* Remove all the schedule that belong to a user
+     * useful when we want to remove a user.
+     * @param {String} id of the user
+     * @return {Int} number of deleted document -> incase we want to do further loging
+     */
+    removeAllSchedule: async (concreteUser) => {
+        console.log("here");
+        let report = await Schedule.deleteMany({
+            user: concreteUser
+        });
+
+        if(report.ok != 1){
+            console.log("Error: occur when delete schedule, of id:" + concreteUser + "!!!!");
+        
+        }return report.deletedCount;
+    },
+
+    /* Remove all the schedule that belong to a user between the start and end period
+     * useful when we want to clear schedule day
+     * @param {String} id of the user
+     * @param {Int} the unix time indicate the starting of the period
+     * @param {Int} the unix time indicate the ending of the period
+     * @return {Int} number of deleted document -> incase we want to do further loging
+     */
+    removeAllScheduleBetween: async (concreteUser, start, end) => {
+        let report = await Schedule.deleteMany({
+            $and: [
+                {"date": {$gte: Util.extractUnixOfYYYY_MM_DD(start)}},
+                {"date": {$lte: Util.extractUnixOfYYYY_MM_DD(end)}}
+            ],
+            "user": concreteUser
+        });
+        if(report.ok != 1){
+            console.log("Error: occur when delete schedule that is betwen {" + 
+            new Date(Util.extractUnixOfYYYY_MM_DD(start))+"} to {" +
+            + new Date(Util.extractUnixOfYYYY_MM_DD(end)) + "}, of id:" + concreteUser + "!!!!");
+        
+        }return report.deletedCount;
     }
 };
 
-
-// create the schedule
-const createSchedule  = async (date) => {
-    try{
-        const schedule = await Schedule.create({date: date});
-        return schedule;
-    } catch (err){
-        console.log("Error");
-        return null;
-    }
-};
-
-// for now put the auto mail here(just a shell)
-// if user want to start a meeting, and wait for participator to vote for a time
-const autoMailCallBack = (contacts, title) => {};
-
-let sche = createSchedule(Date.now);
+module.exports = scheduleController;
