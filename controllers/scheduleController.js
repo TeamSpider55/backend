72773dde6a129f3f8a123116b9c66f997e5cd377
June-Tr <june-tr@outlook.com>
2021-08-23 23:55:42 +1000

    Update scheduleController.js
    rewrite the controller
    -> add + retrieve method

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -23,9 +23,9 @@ let compare = (scheduleA, scheduleB) => {
 const scheduleController = {
    
     /* the URL that will retrieve is:
-     * URL: "../schedule/:date/retrieve"
+     * URL: "../schedule/retrieve/single/:date/:user"
      * req.param = {
-     *   user: {type: String},
+     *   user: {type: int},
      *   date: {type: Int}
      * }
      * @param: {Object}
@@ -36,21 +36,81 @@ const scheduleController = {
         let schedule = null;
         try{
             schedule = await Schedule.findOne({
-                date: Util.extractUnixOfYYYY_MM_DD(req.params.date),
+                // turn a string version of a unix time into the correctly formmated unix time
+                date: Util.extractUnixOfYYYY_MM_DD(parseInt(req.params.date)),
                 user: req.params.user
             });
+
             if(schedule == null) 
+                console.log("Missing document: scheduled day {" + new Date(unixTime) + "}, for id: '" 
+                    + concreteUser + "' is missing from Collection{Schedules}~!!!");
 
-            console.log("Missing document: scheduled day {" + new Date(unixTime) + "}, for id: '" 
-                + concreteUser + "' is missing from Collection{Schedules}~!!!");
         } catch (err){
-            console.log("Retrieve schedule:" + err);
+            console.log("Retrieve schedule: " + err);
 
-        }return await res.Json((schedule == null) ? {} : schedule.Json);
-        
+        }return await res.json((schedule == null) ? {} : schedule);
     },
 
+    /* The URL that will retrieve is
+     * URL: "../schedule/retrieve/many/:user"
+     * req.params = {
+     *     user: {type: String}
+     * }
+     * @param: {Object}
+     * @param: {Object}
+     * @return: {JsonObject} 
+     */
+    retrieveAllSchedulesOfUser: async (req, res) => {
+        let schedules = null;
+        try{
+            schedules = await Schedule.find({
+                user: req.params.user
+            });
 
+            if(schedules == null || schedules.length == 0)
+                console.log("Missing document: scheduled day for id: '" 
+                     + req.params.user + "' is missing from Collection{Schedules}~!!!");
+
+        } catch (err){
+            console.log("Retrieve schedules: " + err);
+        
+        }return await res.json((schedules == null) ? [] : schedules);
+    },
+
+    /* The Url that will be retrieve is 
+     * URL: "../schedule/retrieve/many/:user/:start/:end"
+     * req.params = {
+     *     user: {type: String},
+     *     start: {type: String},
+     *     end: {type:String}
+     * }
+     * @param: {Object}
+     * @param: {Object}
+     * @return: {JsonList}
+     */
+    retrieveAllSchedulesOfUserBetween: async (req, res) => {
+        let schedules = null;
+        try{
+            let start = Util.extractUnixOfYYYY_MM_DD(parseInt(req.params.start));
+            let end = Util.extractUnixOfYYYY_MM_DD(parseInt(req.params.end));
+
+            schedules = await Schedule.find({
+                $and: [
+                    {"date": {$gte: start}},
+                    {"date": {$lte: end}}
+                ],
+                "user": req.params.user
+            });
+
+            if(schedules == null || schedules.length == 0)
+                console.log("Missing document: no scheduled day for id: '" 
+                     + req.params.user + "' between" +  new Date(start) + "-" + new Date(end) +" in Collection{Schedules}~!!!");
+
+        } catch (err){
+            console.log("Retrieve schedules: " + err);
+        
+        } return await res.json((schedules == null) ? [] : schedules);
+    }
 };
 
 
