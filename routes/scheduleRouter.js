8ba9421d07b09da6b1ba7f28da1b26ea728b3a09
June-Tr <june-tr@outlook.com>
2021-08-23 23:55:56 +1000

    write router

--- routes/scheduleRouter.js
+++ routes/scheduleRouter.js
@@ -1,5 +1,9 @@
+const express = require("express");
+const router = express.Router();
 const ScheduleController = require("../controllers/scheduleController");
 
-var express = require("express");
-var router = express.Router();
 
+router.get("/retrieve/single/:date/:user", ScheduleController.retrieveSchedule);
+router.get("/retrieve/many/:user", ScheduleController.retrieveAllSchedulesOfUser);
+router.get("/retrieve/many/:user/:start/:end", ScheduleController.retrieveAllSchedulesOfUserBetween);
+module.exports = router;
