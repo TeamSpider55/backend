5f42e9ff76cf7041dd5529b781e2d92a8ee6c2b4
June-Tr <june-tr@outlook.com>
2021-08-24 05:42:07 +1000

    Reverse the scheduleController
    instead of implement the controller that it return the response.
    
    -> write a router that  has a call back do those task

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,117 +1,167 @@
+const { model } = require("mongoose");
 const Schedule = require("../models/schedules");
 const Util = require("../util");
 
 /* Controll I/O from schedule collection ****************************************************************************************************
  */
-/* Helper function. ******************************************************************************************************************
- */
 
-/* Comparator of the schedule
- * -> this can be use to re organise the retrieve schedule to do calender
- * @param {Schedule} contain date
- * @para, {Schedule} contain date
- * @return {int} 1, if a > b ; -1, if a < b; 0 if a == b
- */
-let compare = (scheduleA, scheduleB) => {
-    if(scheduleA.date < scheduleB.date)
-        return -1;
-    if(scheduleA.date > scheduleB.date)
-        return 1;
-    return 0;
-};
+let scheduleController = {
+    
+    
 
-const scheduleController = {
-   
-    /* the URL that will retrieve is:
-     * URL: "../schedule/retrieve/single/:date/:user"
-     * req.param = {
-     *   user: {type: int},
-     *   date: {type: Int}
-     * }
-     * @param: {Object}
-     * @param: {Object}
-     * @return: {JsonObject} if cant retrieve, the object will be empty
+    /* Add schedule of the user to the data base (schedule collection)
+     * @param {Number} time in millis second that is between 0:0:0 -> 23:59:59.999 of the scheduled day
+     * @param {String} the user id where this schedule belong to
+     * @return {Schedule} the instance scheduleSchema that is stored in data base of null;
      */
-    retrieveSchedule: async (req, res) => {
-        let schedule = null;
+    addSchedule : async (unixTime, concreteUser) => {
         try{
-            schedule = await Schedule.findOne({
-                // turn a string version of a unix time into the correctly formmated unix time
-                date: Util.extractUnixOfYYYY_MM_DD(parseInt(req.params.date)),
-                user: req.params.user
+            return await Schedule.create({
+                date: Util.extractUnixOfYYYY_MM_DD(unixTime),
+                user: concreteUser,
+                events: []
+            });
+        }catch (err){
+            // notify that the new schedule is existed in the db. aka: error code E11000
+            if(err.message.includes("E11000 ")){
+                console.log("Duplicate document: Scheduled day {" + new Date(unixTime) + "}, for id: '"
+                 + concreteUser + "' is existed in Collection{Schedules}~!!!");
+            }else{
+                console.log(err);
+            }
+        } return null;
+    },
+
+
+    /* [done]Retrieve the schedules document using date and user
+     * @param {Number} time in millis second that is between 0:0:0 -> 23:59:59.999 of the scheduled day
+     * @param {String} the user id where this schedule belong to
+     * @return {Schedule} that exist in collection or null
+     */
+    retrieveSchedule: async (unixTime, concreteUser) => {
+        try{
+            let schedule =  await Schedule.findOne({
+                date: Util.extractUnixOfYYYY_MM_DD(unixTime),
+                user: concreteUser
             });
 
-            if(schedule == null) 
+            // log case targetted date is not in database
+            if(schedule == null)
                 console.log("Missing document: scheduled day {" + new Date(unixTime) + "}, for id: '" 
-                    + concreteUser + "' is missing from Collection{Schedules}~!!!");
-
-        } catch (err){
-            console.log("Retrieve schedule: " + err);
-
-        }return await res.json((schedule == null) ? {} : schedule);
+                + concreteUser + "' is missing from Collection{Schedules}~!!!");
+            return schedule;
+            }catch (err){
+            console.log(err);
+        }return null;
     },
 
-    /* The URL that will retrieve is
-     * URL: "../schedule/retrieve/many/:user"
-     * req.params = {
-     *     user: {type: String}
-     * }
-     * @param: {Object}
-     * @param: {Object}
-     * @return: {JsonObject} 
+    /* Retrieve all the schedule belong to a user
+     * @param {String} the id of the user
+     * @return {Schedule[]} contain all the schedule day of the user
      */
-    retrieveAllSchedulesOfUser: async (req, res) => {
-        let schedules = null;
+    retrieveAllScheduleByUser: async (concreteUser) => {
         try{
-            schedules = await Schedule.find({
-                user: req.params.user
+            let schedule =  await Schedule.find({
+                user: concreteUser
             });
 
-            if(schedules == null || schedules.length == 0)
-                console.log("Missing document: scheduled day for id: '" 
-                     + req.params.user + "' is missing from Collection{Schedules}~!!!");
-
-        } catch (err){
-            console.log("Retrieve schedules: " + err);
-        
-        }return await res.json((schedules == null) ? [] : schedules);
+            // log case targetted date is not in database
+            if(schedule == null)
+                console.log("Missing document: scheduled day {" + new Date(unixTime) + "}, for id: '" + concreteUser 
+                + "' is missing from Collection{Schedules}~!!!");
+            return schedule;
+        }catch (err){
+            console.log(err);
+        }return null;
     },
 
-    /* The Url that will be retrieve is 
-     * URL: "../schedule/retrieve/many/:user/:start/:end"
-     * req.params = {
-     *     user: {type: String},
-     *     start: {type: String},
-     *     end: {type:String}
-     * }
-     * @param: {Object}
-     * @param: {Object}
-     * @return: {JsonList}
+    /* Retrieve all the schedules that is between a indicated period of a user
+     * @param {String} the id of the user
+     * @param {Int} the unix time of the starting period
+     * @param {Int} the unix time of the ending period
+     * @return {schedule[]} 
      */
-    retrieveAllSchedulesOfUserBetween: async (req, res) => {
-        let schedules = null;
+    retrieveAllScheduleBetween: async(concreteUser, start, end) => {
         try{
-            let start = Util.extractUnixOfYYYY_MM_DD(parseInt(req.params.start));
-            let end = Util.extractUnixOfYYYY_MM_DD(parseInt(req.params.end));
-
-            schedules = await Schedule.find({
+            let schedules = await Schedule.find({
                 $and: [
-                    {"date": {$gte: start}},
-                    {"date": {$lte: end}}
+                    {"date": {$gte: Util.extractUnixOfYYYY_MM_DD(start)}},
+                    {"date": {$lte: Util.extractUnixOfYYYY_MM_DD(end)}}
                 ],
-                "user": req.params.user
+                "user": concreteUser
             });
 
-            if(schedules == null || schedules.length == 0)
-                console.log("Missing document: no scheduled day for id: '" 
-                     + req.params.user + "' between" +  new Date(start) + "-" + new Date(end) +" in Collection{Schedules}~!!!");
+            if(schedules == null || schedules.length == 0){
+                console.log("No document: scheduled day that between:{" 
+                + new Date(Util.extractUnixOfYYYY_MM_DD(start)) + "} -> {" + 
+                new Date(Util.extractUnixOfYYYY_MM_DD(end)) + "}, for id: '" + concreteUser + "' contain no document~!!!");
+            }
+            return schedules;
+        }catch (err){
+            console.log(err);
+        }
+    },
 
-        } catch (err){
-            console.log("Retrieve schedules: " + err);
+    /* Remove a schedule from the collection using time+ user
+     * @param {Int} the unix time of the schedule
+     * @param {String} the id of the user 
+     * @return {bool} true if the delete is successful
+     */
+    removeSchedule: async (unixTime, concreteUser) => {
         
-        } return await res.json((schedules == null) ? [] : schedules);
+        let report = await Schedule.deleteOne({
+            date: Util.extractUnixOfYYYY_MM_DD(unixTime),
+            user: concreteUser
+        });
+
+        if(report.n == 1)
+            return true;
+
+        if(report.ok != 1)
+            console.log("Error: occur when delete schedule at {" + new Date(Util.extractUnixOfYYYY_MM_DD(unixTime))) + 
+                    ", of id:" + concreteUser + "!!!!";
+
+        return false;
+    },
+
+    /* Remove all the schedule that belong to a user
+     * useful when we want to remove a user.
+     * @param {String} id of the user
+     * @return {Int} number of deleted document -> incase we want to do further loging
+     */
+    removeAllSchedule: async (concreteUser) => {
+        let report = await Schedule.deleteMany({
+            user: concreteUser
+        });
+
+        if(report.ok != 1){
+            console.log("Error: occur when delete schedule, of id:" + concreteUser + "!!!!");
+        
+        }return report.deletedCount;
+    },
+
+    /* Remove all the schedule that belong to a user between the start and end period
+     * useful when we want to clear schedule day
+     * @param {String} id of the user
+     * @param {Int} the unix time indicate the starting of the period
+     * @param {Int} the unix time indicate the ending of the period
+     * @return {Int} number of deleted document -> incase we want to do further loging
+     */
+    removeAllScheduleBetween: async (concreteUser, start, end) => {
+        let report = await Schedule.deleteMany({
+            $and: [
+                {"date": {$gte: Util.extractUnixOfYYYY_MM_DD(start)}},
+                {"date": {$lte: Util.extractUnixOfYYYY_MM_DD(end)}}
+            ],
+            "user": concreteUser
+        });
+        if(report.ok != 1){
+            console.log("Error: occur when delete schedule that is betwen {" + 
+            new Date(Util.extractUnixOfYYYY_MM_DD(start))+"} to {" +
+            + new Date(Util.extractUnixOfYYYY_MM_DD(end)) + "}, of id:" + concreteUser + "!!!!");
+        
+        }return report.deletedCount;
     }
 };
-
 
 module.exports = scheduleController;
--- routes/scheduleRouter.js
+++ routes/scheduleRouter.js
@@ -3,7 +3,78 @@ const router = express.Router();
 const ScheduleController = require("../controllers/scheduleController");
 
 
-router.get("/retrieve/single/:date/:user", ScheduleController.retrieveSchedule);
-router.get("/retrieve/many/:user", ScheduleController.retrieveAllSchedulesOfUser);
-router.get("/retrieve/many/:user/:start/:end", ScheduleController.retrieveAllSchedulesOfUserBetween);
+router.get("/retrieve/single/:date/:user",(req, res) => {
+    let unixTime = parseInt(req.params.date);
+    let user = req.params.user;
+    let schedule = ScheduleController.retrieveSchedule(unixTime, user);
+
+    res.json((schedule == null) ?  {}: schedule);
+});
+
+
+router.get("/retrieve/many/:user", (req, res) => {
+    let user = req.params.user;
+    let schedules = ScheduleController.retrieveAllScheduleByUser(user);
+
+    res.json((schedules == null) ? [] : schedules);
+});
+
+
+
+router.get("/retrieve/many/:user/:start/:end", (req, res) => {
+    let concreteUser = req.params.user;
+    let start = req.params.start;
+    let end = req.params.end;
+
+    let schedules = ScheduleController.retrieveAllScheduleBetween(concreteUser, start, end);
+
+    res.json((schedules == null) ? [] : schedules);
+});
+
+
+
+router.post("/add", (req, res) => {
+    let unixTime = parseInt(req.body.date);
+    let concreteUser = req.body.user;
+
+    if(ScheduleController.addSchedule(unixTime, concreteUser) != null){
+        console.log("Add Schedule: successfully!!!");
+    }
+});
+
+
+
+router.post("/remove/single", (req, res) => {
+    let unixTime = parseInt(req.body.date);
+    let concreteUser = req.body.user;
+
+    if(ScheduleController.removeSchedule(unixTime, concreteUser)){
+        console.log("Remove Schedule: successfull!!!");
+    }
+});
+
+
+
+router.post("/remove/many/user", (req, res) => {
+    let concreteUser = req.body.user;
+
+    let deleteNum = ScheduleController.removeAllSchedule(concreteUser);
+
+    if(deleteNum > 0)
+        console.log("Remove Schedule: " + deleteNum + " is removed from the collection!!!");
+});
+
+
+router.post("/remove/many/user/between", (req, res) => {
+    let concreteUser = req.body.user;
+    let start = parseInt(req.body.start);
+    let end = parseInt(req.body.end);
+
+    
+    let deleteNum = ScheduleController.removeAllScheduleBetween(concreteUser, start, end);
+
+    if(deleteNum > 0)
+        console.log("Remove Schedule: " + deleteNum + " is removed from the collection!!!");
+});
+
 module.exports = router;
