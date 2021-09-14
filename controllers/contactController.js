3420ecc30fbc6dd3212a017561c60012e871f501
Justin-Chen1010 <chenjustin1010@gmail.com>
2021-09-14 23:37:48 +1000

    Added prototype update contact and delete contact
    Added 2 functions in, have not done any testing yet.

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -40,6 +40,97 @@ const getOneContact = async (req, res) => {
     })
   }
 }
+
+// change an order (POST)
+const updateContact = async (req, res) => {
+  try {
+    const newContact = req.body //this has all the attributes
+    const oneContact = await Contact.findOne({ _id: id }).lean()
+    if (oneContact === null) {
+      // no contact found in database
+      res.status(404)
+      return res.json({
+        statusCode: 404,
+      })
+    }
+    // update the contact's details
+    oneContact.nickName = newContact.nickName
+    oneContact.familyName = newContact.familyName
+    oneContact.middleName = newContact.middleName
+    oneContact.givenName = newContact.givenName
+    oneContact.email = newContact.email
+    oneContact.phone = newContact.phone
+    oneContact.address = newContact.address
+    oneContact.description = newContact.description
+    oneContact.note = newContact.note
+    oneContact.tags = newContact.tags.map((tag) => ({
+      //Do we map for tags array? how do we do it if they are changing the other stuff
+      _id: mongoose.Types.ObjectId(),
+      tagId: tag.tagId,
+      description: tag.description,
+      color: tag.color,
+      priority: tag.priority,
+    }))
+    let result = await oneContact.save()
+    // contact was found, return as response
+    return res.json({
+      statusCode: 200,
+      data: result,
+    })
+  } catch (err) {
+    console.log(err)
+    // error occurred
+    res.status(400)
+    return res.json({
+      statusCode: 400,
+      data: err,
+    })
+  }
+}
+
+const deleteOneContact = async (req, res) => {
+  const userName = req.body.userName
+  const id = req.body.id
+  try {
+    const user = await User.findOne({
+      userName: userName,
+    })
+    const oneContact = await Contact.findOne({ _id: id }).lean()
+    if (oneContact === null) {
+      // no contact found in database
+      res.status(404)
+      return res.json({
+        statusCode: 404,
+      })
+    }
+    if (user === null) {
+      // no User found in database
+      res.status(404)
+      return res.json({
+        statusCode: 404,
+      })
+    }
+    user.contacts.deleteOne({ _id: id }).lean()
+    //Does this delete? or only the linked in array
+
+    // contact was deleted successfully
+    return res.json({
+      statusCode: 200,
+      data: user,
+    })
+  } catch (err) {
+    console.log(err)
+    // error occurred
+    res.status(400)
+    return res.json({
+      statusCode: 400,
+      data: err,
+    })
+  }
+}
+
 module.exports = {
   getOneContact,
+  updateContact,
+  deleteOneContact,
 }
