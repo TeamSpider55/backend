b15a54dabbcb0199c446eefd22d1dd2cfa55d410
June-Tr <june-tr@outlook.com>
2021-09-15 04:15:09 +1000

    Complete the debug

--- models/contacts.js
+++ models/contacts.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
-<<<<<<< HEAD:models/contact.js
-  userId: { type: String, unique: true }, //Foreign key only unique within the user(?)
-=======
-  clientId: { type: String, required: true, unique: true },
->>>>>>> 98d562be30132f177da5d5353ec0c2ae333a0a31:models/contacts.js
+  ContactId: { type: String, required: true, unique: true },
   nickName: { type: String },
   familyName: { type: String, required: true },
   middleName: { type: String },
