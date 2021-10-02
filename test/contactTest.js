1b4bdb7ce6abcbfbe7a6bfbb3e0d2f9347148cc1
Justin-Chen1010 <80225730+Justin-Chen1010@users.noreply.github.com>
2021-10-02 21:32:19 +1000

    Updated contactAPI testing
    Added the skeleton for the rest of the API testing, 2 or 3 need fixing others (these are commented out) all work fine.

--- test/contactTest.js
+++ test/contactTest.js
@@ -51,3 +51,87 @@ describe('Get contacts for user', () => {
     });
 });
 
+describe('Add Contact, Update Contact, Delete Contact', () => {
+    let newContact;
+    //before( function(){
+    it('Add a contact succesfully', async () => {
+        const result = await axios.post('http://localhost:8080/contact/addContact', {
+            userName: 'spidertest55',
+            email: 'autoTest@autotest.com',
+            givenName: 'auto',
+            familyName: 'testing'
+        });
+        //console.log(result.data.data._id);
+        newContact = result.data.data._id;
+        //console.log(result.data.data);
+        expect(result.data.statusCode).to.be.eq(200);
+    });
+    // after( function() {
+    //     db.collection.findOneAndDelete(newContact);
+    // });
+    //}); TODO:NOT WORKING DB IS UNDEFINED
+
+    //console.log(newContact); TODO: FIX DELETE CONTACT
+    // it('Delete a contact succesfully', async () => {
+    //     const result = await axios.delete('http://localhost:8080/contact/deleteContact', {
+    //         userName: 'spidertest55',
+    //         contactId: '61583e337f881de1b49bac7d'
+    //     });
+    //     console.log(result);
+    //     expect(result.data.statusCode).to.be.eq(200);
+    // });
+
+    it('Fail to add a contact', async () => {
+        try{
+            const result = await axios.post('http://localhost:8080/contact/addContact', {
+            userName: 'spidertest53',
+            email: 'autoTest@autotest.com',
+            givenName: 'auto',
+            familyName: 'testing'
+        });
+        }catch(err){
+            result = err.response;
+        }
+        //console.log(result.data);
+        expect(result.data.statusCode).to.be.eq(404);
+    });
+    // it('Fail to delete a contact', async () => {
+    //     const result = await axios.delete('http://localhost:8080/contact/deleteContact', {
+    //         userName: 'spidertest52',
+    //         contactId: '61583e337f881de1b49bac7d'
+    //     }); TODO: THIS DOESNT WORK EITHER
+    //     expect(result.data.statusCode).to.be.eq(404);
+    // });
+    it('Update a contact succesfully', async () => {
+        const result = await axios.post('http://localhost:8080/contact/updateContact', {
+            contactId: '61556e08e050338e94d23601',
+            nickName: 'autoTest@autotest.com',
+            givenName: 'auto',
+            middleName: 'auto testing',
+            familyName: 'testing',
+            email: 'changed',
+            phone: 'changed',
+            address: 'changed',
+            description: 'changed',
+            note: 'changed',
+        });
+        //console.log(result.data.data);
+        expect(result.data.statusCode).to.be.eq(200);
+    });
+    // it('Fail to update contact', async () => {
+    //     const result = await axios.post('http://localhost:8080/contact/updateContact', {
+    //         contactId: '61556e08e050338e94d23609', TODO: CANT UPDATE BECAUSE OF INVALID ID?
+    //         nickName: 'autoTest@autotest.com',
+    //         givenName: 'auto',
+    //         middleName: 'auto testing',
+    //         familyName: 'testing',
+    //         email: 'changed',
+    //         phone: 'changed',
+    //         address: 'changed',
+    //         description: 'changed',
+    //         note: 'changed',
+    //     });
+    //     //console.log(result.data.data);
+    //     expect(result.data.statusCode).to.be.eq(404);
+    // });
+});
