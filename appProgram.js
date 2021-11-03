9f1641155f49defb9239f7cd9e9f884ce56dee8d
jonjau <jonjau3q@gmail.com>
2021-11-03 18:10:01 +1100

    insecure cookies for localhost testing

--- appProgram.js
+++ appProgram.js
@@ -12,7 +12,22 @@ const scheduleRouter = require("./routes/scheduleRouter");
 const eventRouter = require("./routes/eventRouter");
 const userRouter = require("./routes/userRouter");
 
-app.use(cors());
+const whitelist = ['http://localhost:3000/', 'http://localhost:8080/'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    if(!origin) return callback(null, true);
+    if(whitelist.indexOf(origin) === -1){
+      var message = 'The CORS policy for this origin doesn\'t ' +
+                'allow access from the particular origin.';
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
