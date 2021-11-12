4365520e901d51e9d27035ee318c81d860536c74
junetrinh <xtrinh@student.unimelb.edu.au>
2021-11-12 16:52:57 +1100

    update

--- routes/scheduleRouter.js
+++ routes/scheduleRouter.js
@@ -2,27 +2,30 @@ const express = require("express");
 const router = express.Router();
 const ScheduleController = require("../controllers/scheduleController");
 
+const passport = require("passport");
+router.use(passport.authenticate("jwt", { session: false }));
+
 /* Send the path with URL parameter to indicate that we want to retrieve
  * a single schedule with the date and belong to a user
  */
-router.get("/retrieve/single/:date/:user", async (req, res) => {
+router.get("/retrieve/single/:date", async (req, res) => {
     let unixTime = parseInt(req.params.date);
-    let user = req.params.user;
+    let user = req.user._id;
     res.json(await ScheduleController.retrieveSchedule(unixTime, user));
 });
 
 
 /*
  */
-router.get("/retrieve/many/:user", async(req, res) => {
-    let user = req.params.user;
+router.get("/retrieve/many", async(req, res) => {
+    let user = req.user._id;
     res.json( await ScheduleController.retrieveAllScheduleByUser(user));
 });
 
 
 
-router.get("/retrieve/many/:user/:start/:end", async(req, res) => {
-    let concreteUser = req.params.user;
+router.get("/retrieve/many/:start/:end", async(req, res) => {
+    let concreteUser = req.user._id;
     let start = req.params.start;
     let end = req.params.end;
 
@@ -34,7 +37,7 @@ router.get("/retrieve/many/:user/:start/:end", async(req, res) => {
 
 router.post("/add", async(req, res) => {
     let unixTime = parseInt(req.body.date);
-    let concreteUser = req.body.user;
+    let concreteUser = req.user._id;
     res.json( await ScheduleController.addSchedule(unixTime, concreteUser));
         
 });
@@ -43,7 +46,7 @@ router.post("/add", async(req, res) => {
 
 router.post("/remove/single", async(req, res) => {
     let unixTime = parseInt(req.body.date);
-    let concreteUser = req.body.user;
+    let concreteUser = req.user._id;
 
     res.json( await ScheduleController.removeSchedule(unixTime, concreteUser));
 });
@@ -51,14 +54,14 @@ router.post("/remove/single", async(req, res) => {
 
 
 router.post("/remove/many/user", async(req, res) => {
-    let concreteUser = req.body.user;
+    let concreteUser = req.user._id;
 
     res.json(await ScheduleController.removeAllSchedule(concreteUser));
 });
 
 
 router.post("/remove/many/user/between", async (req, res) => {
-    let concreteUser = req.body.user;
+    let concreteUser = req.user._id;
     let start = parseInt(req.body.start);
     let end = parseInt(req.body.end);
 
