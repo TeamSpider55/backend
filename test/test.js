4b46c5cc47942c909e686b1dca14decec886660b
junetrinh <xtrinh@student.unimelb.edu.au>
2021-11-12 15:45:40 +1100

    manually merger the NewParticipatorAutomator

--- test/test.js
+++ test/test.js
@@ -354,4 +354,215 @@ describe('Test Authentication API', function() {
             expect(result.data.statusCode).to.be.eq(404);
         });
     });
+
+    let hourCollection = [];
+    before(() => {
+        for(var i= 0; i < 240; i+= 20){
+            hourCollection.push((i) * 1000 * 60 * 6) ;
+        }
+    });
+    describe(
+        "Unit testing for the event API!!!", function(){
+            // testing GET request to user profile when provided the authentication cookie
+            // testing GET request to user profile when provided the authentication cookie
+            let dataInCache = (data) => {
+                for(var i of inputInDb){
+                    if(data.length == i.lenth){
+                        for(var attribute in i){
+                            if(i.attribute != data.attribute){
+                                return false;
+                            }
+                        }return true;
+                    }
+                }return false;
+            }
+            let inputInDb = [];
+            let token;
+
+            
+            
+            describe( 
+                
+                "Assert the correctness of the 'Add' route",
+                function(){
+                    beforeEach(async function() {
+                        const result = await axios.post('http://localhost:5050/auth/login', {
+                            id: '123',
+                            password: '123'
+                        })
+                        token = result.headers['set-cookie'][0].split(';')[0].slice(4);
+                    });
+                    it(
+                        "[Expect: success] Enter an event to a User who does not have that schedule with the same date",
+                        async() => {
+                            const data = {
+                                "title": "test",
+                                "note": "test",
+                                "start": hourCollection[0] + 1000 * 60 * 6,
+                                "end": hourCollection[0] + 1500 * 60 * 6,
+                                "type": "personal",
+                            };
+                            let res = await axios.post(
+                                'http://localhost:5050/event/add',
+                                data,{
+                                    headers: { 
+                                        Cookie:`CRM=${token}` 
+                                    }
+                                },
+                                { withCredentials: true }
+                                );
+                            
+                            expect(res.data.status.statusCode).to.deep.equal(200);
+                            inputInDb.push({
+                                "start": data.start,
+                                "end": data.end
+                            });
+                        }
+                    );
+                    it(
+                        "[Expect: fail]  Enter an event which should be existed in database!!",
+                        async() => {
+                            const data = {
+                                "title": "test",
+                                "note": "test",
+                                "start": hourCollection[0] + 1000 * 60 * 6,
+                                "end": hourCollection[0] + 1500 * 60 * 6,
+                                "type": "personal",
+                            };
+                            let res = await axios.post(
+                                'http://localhost:5050/event/add',
+                                data, {
+                                    headers: { 
+                                        Cookie:`CRM=${token}` 
+                                    }
+                                },
+                                { withCredentials: true }
+                                );
+                            expect(res.data.status.statusCode).to.deep.equal(400);
+                        }
+                    )
+                    it(
+                        "[Expect: success] Enter a list of event of a user",
+                        async() => {
+                            for (var i =1; i< hourCollection.length;i++){
+                                const data = {
+                                    "title": "test",
+                                    "note": "test",
+                                    "start": hourCollection[i] + 1000 * 60 * 6,
+                                    "end": hourCollection[i] + 1500 * 60 * 6,
+                                    "type": "personal",
+                                    "tags": [],
+                                    "contacts":[]
+                                };
+                                let res = await axios.post(
+                                    'http://localhost:5050/event/add',
+                                    data, {
+                                        headers: { 
+                                            Cookie:`CRM=${token}` 
+                                        }
+                                    },
+                                    { withCredentials: true }
+                                    );
+                                
+                                expect(res.data.status.statusCode).to.deep.equal(200);
+                                inputInDb.push({
+                                    "start": data.start,
+                                    "end": data.end
+                                });
+                            }
+                        }
+                    );
+                }
+            );
+            
+            describe(
+                'Assert the correctness of "Modified" route',
+                function() {
+                    beforeEach(async function() {
+                        const result = await axios.post('http://localhost:5050/auth/login', {
+                            id: '123',
+                            password: '123'
+                        })
+                        token = result.headers['set-cookie'][0].split(';')[0].slice(4);
+                    });
+                    it(
+                        "[Expect: success] modified an events ~!!!!", 
+                        async () => {
+                            var data = {
+                                "title": "b",
+                                "note": "b",
+                                "start": hourCollection[0] + 1000 * 60 * 6,
+                                "end": hourCollection[0] + 1500 * 60 * 6,
+                                "type": "personal"
+                            };
+                            let res = await axios.post(
+                                'http://localhost:5050/event/modify/content',
+                                data, {
+                                    headers: { 
+                                        Cookie:`CRM=${token}` 
+                                    }
+                                },
+                                { withCredentials: true }
+                                );
+                            // console.log(res.data);
+                            expect(res.data.statusCode).to.deep.equal(200);
+                        }
+                    );
+                }
+            );
+            describe(
+                'Assert the correctness of "Remove" route',
+                function() {
+                    beforeEach(async function() {
+                        const result = await axios.post('http://localhost:5050/auth/login', {
+                            id: '123',
+                            password: '123'
+                        })
+                        token = result.headers['set-cookie'][0].split(';')[0].slice(4);
+                    });
+                    it(
+                        "[Expect: success] Remove all the event added to the data base ~!!!!", 
+                        async () => {
+                            for(var cached of inputInDb){
+                                var data = {
+                                    "start": cached.start,
+                                    "end":cached.end
+                                };
+                                let res = await axios.post(
+                                    'http://localhost:5050/event/remove',
+                                    data, {
+                                        headers: { 
+                                            Cookie:`CRM=${token}` 
+                                        }
+                                    },
+                                    { withCredentials: true }
+                                    );
+                                // console.log(res.data.statusCode);
+                                expect(res.data.statusCode).to.deep.equal(200);
+                            }
+                    });
+                    it(
+                        "[Expect: fail] Remove an event that is not in data base ~!!!!", 
+                        async () => {
+                            
+                            var data = {
+                                "start": hourCollection[0] + 1000 * 60 * 6,
+                                "end": hourCollection[0] + 1500 * 60 * 6,
+                            };
+                            let res = await axios.post(
+                                'http://localhost:5050/event/remove',
+                                data, {
+                                    headers: { 
+                                        Cookie:`CRM=${token}` 
+                                    }
+                                },
+                                { withCredentials: true }
+                                );
+                            // console.log(res.data.statusCode);
+                            expect(res.data.statusCode).to.deep.equal(400);
+                            
+                    });
+                }
+            );
+        }
 });
